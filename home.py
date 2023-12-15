b6b7745990f24daefa577b8ce57edd65ca0ba759
teerapat2002 <teerapat11002@gmail.com>
2023-12-15 14:39:00 +0700

    str01

--- home.py
+++ home.py
@@ -1,2 +1,22 @@
-import streamlit as st;
-st.header("teerapat Satitsatenwong")
+import streamlit as st
+
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('นายธีรภัทร สถิตเสถียรวงศ์')
+st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/flower.jpg')
+with col2:
+    st.image('./pic/profile.jpg')
+
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
